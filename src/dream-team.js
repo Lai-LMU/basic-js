043b3a12761c488d44de2f96253a717530ca5e32
Anastasia_Poleshuk <71152591+Lai-LMU@users.noreply.github.com>
2021-03-28 20:48:43 +0300

    feat: completed dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam( members) {
+  let str = '';
+  if( members == null){
+    return false;
+  }
+  for(let i=0; i< members.length; i++){
+    if(typeof members[i] == "string"){
+      if(members[i] != null){
+        members[i] =  members[i].trim();
+      }
+        str += members[i][0].toUpperCase();
+    } 
+  }
+  if(str == null || str.length == 0){
+    return false;
+  } else{
+    return alphabetize(str);
+  }
+
 };
+
+function alphabetize(str) {
+  return str.split('').sort().join('');
+}
