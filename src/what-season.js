2f6fba859424d32b5b91f7f6e14906085db81d53
Anastasia_Poleshuk <71152591+Lai-LMU@users.noreply.github.com>
2021-03-28 23:49:16 +0300

    feat: completed what-seazon task

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,55 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason( date) {
+  if(typeof date != "object" || date == undefined || date.length == 0){
+    return 'Unable to determine the time of year!';
+  }
+
+  if( checkDate(date) == false){
+    throw Error;
+  }
+
+ let seazon = date.getMonth();
+ let arrWinter = [11, 0, 1];
+ let arrSpring = [2, 3, 4];
+ let arrSummer = [5, 6, 7];
+ let arrAutumn = [8, 9, 10];
+
+ for(let i=0; i< arrWinter.length; i++){
+   if(arrWinter[i] == seazon){
+     return 'winter';
+   }
+ };
+ for(let j=0; j< arrSpring.length; j++){
+  if(arrSpring[j] == seazon){
+    return 'spring';
+  }
 };
+for(let k=0; k< arrSummer.length; k++){
+  if(arrSummer[k] == seazon){
+    return 'summer';
+  }
+};
+for(let l=0; l< arrAutumn.length; l++){
+  if(arrAutumn[l] == seazon){
+    return ('autumn'|| 'fall');
+  }
+};
+};
+
+function checkDate (date){
+   let l = date.toString();
+   let year = '';
+   let day = '';
+   for(let i= 11; i<=14; i++){
+     year += l[i];
+   }
+   for(let j= 8; j<=9; j++){
+    day += l[j];
+  }
+  if(date.getDate() != +day || date.getFullYear()!= +year){
+    return false;
+  }else{
+    return true;
+  }
+}
